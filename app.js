1e67bbcff758ee049474b8926942fb496e0d4465
Rishabh Kumar Choudhary <97455703+rk26072003@users.noreply.github.com>
2024-05-24 21:47:23 +0530

    added cors

--- app.js
+++ app.js
@@ -4,8 +4,15 @@ import { createServer } from 'http'
 import  healthcheckRoute  from './routes/healthcheck.route.js'
 import  userRoutes  from './routes/user.routes.js'
 import cookieParser from 'cookie-parser'
+import cors from 'cors'
+
+const corsOptions = {
+    origin: 'http://localhost:3000',
+    optionsSuccessStatus: 200,
+};
 
 const app = express();
+app.use(cors(corsOptions));
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
