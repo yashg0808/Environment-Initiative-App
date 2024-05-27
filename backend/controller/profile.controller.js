d7816e879a5eaecc8ea1a84c55b4f99881983a23
Rishabh Kumar Choudhary <97455703+rk26072003@users.noreply.github.com>
2024-05-27 09:11:19 +0530

    completed Profile Routes.

--- backend/controller/profile.controller.js
+++ backend/controller/profile.controller.js
@@ -1,6 +1,84 @@
 import { Profile } from "../models/profile.model.js";
+import { User } from "../models/user.model.js";
+import { ApiError } from "../utils/ApiError.js";
 
-const getMyProfile = asyncHandler(async (req, res) => {
+const getUserSocialProfile = async (userId, req) => {
+    const user = await User.findById(userId);
+    if (!user) {
+        throw new ApiError(404, "User does not exist");
+    }
+    let profile = await SocialProfile.aggregate([
+        {
+            $match: {
+              owner: new mongoose.Types.ObjectId(userId),
+            },
+        },
+        {
+            $lookup: {
+              from: "users",
+              localField: "owner",
+              foreignField: "_id",
+              as: "account",
+              pipeline: [
+                {
+                  $project: {
+                    avatar: 1,
+                    username: 1,
+                    email: 1,
+                    isEmailVerified: 1,
+                  },
+                },
+              ],
+            },
+        },
+        {
+            $lookup: {
+              from: "socialfollows",
+              localField: "owner",
+              foreignField: "followerId",
+              as: "following", // users that are followed by current user
+            },
+        },
+        {
+            $lookup: {
+              from: "socialfollows",
+              localField: "owner",
+              foreignField: "followeeId",
+              as: "followedBy", // users that are following the current user
+            },
+        },
+        {
+            $addFields: {
+              account: { $first: "$account" },
+              followersCount: { $size: "$followedBy" },
+              followingCount: { $size: "$following" },
+            },
+        },
+        {
+            $project: {
+              followedBy: 0,
+              following: 0,
+            },
+        },
+    ]);
+    let isFollowing = false;
+    if (req.user?._id && req.user?._id?.toString() !== userId.toString()) {
+        // Check if there is a logged in user and logged in user is NOT same as the profile that is being loaded
+        // In such case we will check if the logged in user follows the loaded profile user
+        const followInstance = await SocialFollow.findOne({
+          followerId: req.user?._id, // logged in user. If this is null `isFollowing` will be false
+          followeeId: userId,
+        });
+        isFollowing = followInstance ? true : false;
+    }
+    const userProfile = profile[0];
+    if (!userProfile) {
+        throw new ApiError(404, "User profile does not exist");
+    }
+    return { ...userProfile, isFollowing };
+}
+
+export const getMyProfile = asyncHandler(async (req, res) => {
     let profile = await Profile.findOne({
         owner: req.user._id,
     });
@@ -31,3 +109,58 @@ export const updateProfile = asyncHandler(async (req, res) => {
         .status(200)
         .json(new ApiResponse(200, profile, "User profile updated successfully"));
 });
+
+
+// Public route
+export const getProfileByUserName = asyncHandler(async (req, res) => {
+    const { username } = req.params;
+  
+    const user = await User.findOne({ username });
+  
+    if (!user) {
+      throw new ApiError(404, "User does not exist");
+    }
+  
+    const userProfile = await getUserSocialProfile(user._id, req);
+  
+    return res
+      .status(200)
+      .json(
+        new ApiResponse(200, userProfile, "User profile fetched successfully")
+      );
+});
+
+export const updateCoverImage = asyncHandler(async (req, res) => {
+    // Check if user has uploaded a cover image
+    if (!req.file?.filename) {
+      throw new ApiError(400, "Cover image is required");
+    }
+    const coverImageUrl = getStaticFilePath(req, req.file?.filename);
+    const coverImageLocalPath = getLocalPath(req.file?.filename);
+    const profile = await Profile.findOne({
+      owner: req.user._id,
+    });
+    let updatedProfile = await Profile.findOneAndUpdate(
+      {
+        owner: req.user._id,
+      },
+      {
+        $set: {
+          coverImage: {
+            url: coverImageUrl,
+            localPath: coverImageLocalPath,
+          },
+        },
+      },
+      { new: true }
+    );
+  
+    // remove the old cover image
+    removeLocalFile(profile.coverImage.localPath);
+    updatedProfile = await getUserSocialProfile(req.user._id, req);
+    return res
+      .status(200)
+      .json(
+        new ApiResponse(200, updatedProfile, "Cover image updated successfully")
+      );
+  });
--- backend/routes/profile.routes.js
+++ backend/routes/profile.routes.js
@@ -2,22 +2,23 @@ import { Router } from "express";
 import { getLoggedInUserOrIgnore, verifyJWT } from "../middlewares/auth.middleware.js";
 import { validate } from "../validators/validate.js";
 import { upload } from "../middlewares/multer.middleware.js";
-import { updateProfile } from "../controller/profile.controller.js";
+import { getMyProfile, getProfileByUserName, updateCoverImage, updateProfile } from "../controller/profile.controller.js";
 import { updateSocialProfileValidator } from "../validators/profile.validator.js";
 
 const router = Router();
 
 // This will be a public route
-// router.route("/u/:username").get(
-//     getLoggedInUserOrIgnore, // hover over the middleware to know more
-//     getProfileByUserNameValidator(),
-//     validate,
-// );
+router.route("/u/:username").get(
+    getLoggedInUserOrIgnore, // hover over the middleware to know more
+    getProfileByUserNameValidator(),
+    validate,
+    getProfileByUserName
+);
 
 router.use(verifyJWT);
 router.route("/")
-    .get()
+    .get(getMyProfile)
     .patch(updateSocialProfileValidator(),validate, updateProfile);
 router.route("/my-initiatives").get()
-router.route("/cover-image").patch(upload.single("coverImage"));
+router.route("/cover-image").patch(upload.single("coverImage"),updateCoverImage);
 export default router;
