a4941c88cbdcfd202590304dea7b80698ee47e10
Rishabh Kumar Choudhary <97455703+rk26072003@users.noreply.github.com>
2024-06-05 16:35:01 +0530

    added vercel

--- backend/app.js
+++ backend/app.js
@@ -32,13 +32,6 @@ const httpServer = createServer(app);
 
 app.use(express.static("public")); // configure static file to save images locally
 
-// Middleware to enable CORS
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "http://localhost:3000"); // Allow requests from localhost:3000
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-});
-
 app.use("/api/v1/healthcheck", healthcheckRoute);
 
 // Routes required:
