a1d4c8572169f8bf05ab7e4d5f4bfec74e8c85e8
Yash Gupta <yash.gupta0808@gmail.com>
2024-06-23 17:54:10 +0530

    final commit

--- frontend/src/pages/support/SupportPageContainer.jsx
+++ frontend/src/pages/support/SupportPageContainer.jsx
@@ -3,6 +3,7 @@ import { useParams } from "react-router-dom";
 import Web3 from "web3";
 import AuthService from "../../services/auth/AuthService";
 import ApiError from "../../services/ApiError";
+import { contractAddress, abi } from "../../constants";
 
 const SupportPageContainer = () => {
   const { initiativeId } = useParams();
@@ -12,131 +13,6 @@ const SupportPageContainer = () => {
   const [donationAmount, setDonationAmount] = useState("");
   const [userId, setUserId] = useState("");
   const [statusMessage, setStatusMessage] = useState("");
-
-  const contractAddress = "0x4Abc6CFe0EA35AaA8576E67e301fBc76006387b2"; // Replace with your actual contract address
-
-  const contractABI = [
-    {
-      "inputs": [
-        {
-          "internalType": "string",
-          "name": "initiativeId",
-          "type": "string"
-        },
-        {
-          "internalType": "string",
-          "name": "userId",
-          "type": "string"
-        }
-      ],
-      "name": "donate",
-      "outputs": [],
-      "stateMutability": "payable",
-      "type": "function"
-    },
-    {
-      "inputs": [],
-      "stateMutability": "nonpayable",
-      "type": "constructor"
-    },
-    {
-      "anonymous": false,
-      "inputs": [
-        {
-          "indexed": true,
-          "internalType": "address",
-          "name": "from",
-          "type": "address"
-        },
-        {
-          "indexed": false,
-          "internalType": "string",
-          "name": "initiativeId",
-          "type": "string"
-        },
-        {
-          "indexed": false,
-          "internalType": "string",
-          "name": "userId",
-          "type": "string"
-        },
-        {
-          "indexed": false,
-          "internalType": "uint256",
-          "name": "amount",
-          "type": "uint256"
-        }
-      ],
-      "name": "FundsReceived",
-      "type": "event"
-    },
-    {
-      "anonymous": false,
-      "inputs": [
-        {
-          "indexed": true,
-          "internalType": "address",
-          "name": "to",
-          "type": "address"
-        },
-        {
-          "indexed": false,
-          "internalType": "uint256",
-          "name": "amount",
-          "type": "uint256"
-        }
-      ],
-      "name": "FundsWithdrawn",
-      "type": "event"
-    },
-    {
-      "stateMutability": "payable",
-      "type": "fallback"
-    },
-    {
-      "inputs": [
-        {
-          "internalType": "address payable",
-          "name": "receiver",
-          "type": "address"
-        }
-      ],
-      "name": "withdrawFunds",
-      "outputs": [],
-      "stateMutability": "nonpayable",
-      "type": "function"
-    },
-    {
-      "stateMutability": "payable",
-      "type": "receive"
-    },
-    {
-      "inputs": [],
-      "name": "getBalance",
-      "outputs": [
-        {
-          "internalType": "uint256",
-          "name": "",
-          "type": "uint256"
-        }
-      ],
-      "stateMutability": "view",
-      "type": "function"
-    },
-    {
-      "inputs": [],
-      "name": "owner",
-      "outputs": [
-        {
-          "internalType": "address",
-          "name": "",
-          "type": "address"
-        }
-      ],
-      "stateMutability": "view",
-      "type": "function"
-    }
-  ];
 
   useEffect(() => {
     const fetchUser = async () => {
@@ -158,7 +34,7 @@ const SupportPageContainer = () => {
           setAccount(accounts[0]);
           setWeb3(web3Instance);
           const contractInstance = new web3Instance.eth.Contract(
-            contractABI,
+            abi,
             contractAddress
           );
           setContract(contractInstance);
@@ -171,7 +47,7 @@ const SupportPageContainer = () => {
         const accounts = await web3Instance.eth.getAccounts();
         setAccount(accounts[0]);
         const contractInstance = new web3Instance.eth.Contract(
-          contractABI,
+          abi,
           contractAddress
         );
         setContract(contractInstance);
